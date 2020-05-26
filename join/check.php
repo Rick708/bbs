e682298c229a9127c4bf96e8f535b10173bb8273
Rick708 <56381794+Rick708@users.noreply.github.com>
2020-05-26 23:43:05 +0900

    user登録

--- join/check.php
+++ join/check.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if (!isset($_session['join'])){
+	header('Location: index.php');
+	exit();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
--- join/index.php
+++ join/index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 if (!empty($_POST)){
 	if ($_POST['name'] === '') {
 		$error['name'] = 'blank';
@@ -12,8 +14,8 @@ if (!empty($_POST)){
 	if ($_POST['password'] === '') {
 		$error['password'] = 'blank';
 	}
-
 	if (empty($error)){
+		$_session['join'] = $_POST;
 		header('Location: check.php');
 		exit();
 	}
